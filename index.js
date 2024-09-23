fe930dc348bc3820fb113d77533045ce39f750a6
Basavaraj jewargi <6177.raj@gmail.com>
2024-09-23 16:31:29 +0530

    added

---
+++ index.js
@@ -0,0 +1,22 @@
+function playy(){
+var vara;
+vara = Math.floor(Math.random() * 6) + 1;
+let a="images/"+vara+".png";
+document.querySelector(".dice1").setAttribute("src",a);
+
+var varaa;
+varaa = Math.floor(Math.random() * 6) + 1;
+let aa="images/"+varaa+".png";
+document.querySelector(".dice2").setAttribute("src",aa);
+
+
+if(vara<varaa){
+    document.querySelector("h1").innerHTML="Player 1 wins!!";
+}else if(vara>varaa){
+    document.querySelector("h1").innerHTML="Player 2 wins!!";
+
+}else{
+    document.querySelector("h1").innerHTML="Draw!!!!";
+
+}
+}
